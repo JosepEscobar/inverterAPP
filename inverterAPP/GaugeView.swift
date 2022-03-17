02a30699fc6fe30005e59637a2af6352f4883239
Josep Escobar <josepescobarn@gmail.com>
2022-03-17 18:56:21 +0100

    Put hardcoded values into Constant enum

--- inverterAPP/GaugeView.swift
+++ inverterAPP/GaugeView.swift
@@ -4,23 +4,41 @@ struct GaugeView: View {
     @State var progressValue: CGFloat
     @State var titleText: String
     
+    enum Constant {
+        static let cornerRadius: CGFloat = 10
+        static let width: CGFloat = 150
+        static let height: CGFloat = 200
+        static let shadowOpacity: Double = 0.3
+        static let shadowRadius: CGFloat = 10
+        static let shadowXOffset: CGFloat = 0
+        static let shadowYOffset: CGFloat = 0
+
+        enum ProgressBarView {
+            static let width: CGFloat = 156
+            static let height: CGFloat = 145
+        }
+    }
     
     var body: some View {
         VStack{
             ZStack {
                 Color.white
-                    .cornerRadius(10)
+                    .cornerRadius(Constant.cornerRadius)
                 VStack{
                     ProgressBarView(progressValue: progressValue, tokenCharacter: "%%")
-                        .frame(width: 156, height: 145)
+                        .frame(width: Constant.ProgressBarView.width,
+                               height: Constant.ProgressBarView.height)
                     Text(titleText)
                         .font(.caption)
                         .bold()
                         .padding(.horizontal)
                 }
             }
-            .frame(width: 150, height: 200)
-            .shadow(color: Color.black.opacity(0.3), radius: 10, x: 0, y: 0)
+            .frame(width: Constant.width, height: Constant.height)
+            .shadow(color: Color.black.opacity(Constant.shadowOpacity),
+                    radius: Constant.shadowRadius,
+                    x: Constant.shadowXOffset,
+                    y: Constant.shadowYOffset)
         }
     }
 }
